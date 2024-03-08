db15173bab291589ccef6e997e140383c0e62304
Serhii Kotielnikov <s.kotielnikov@m2e.team>
2024-03-08 15:58:46 +0200

    Refactoring image service, removed vue router

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -5,17 +5,22 @@ namespace App\Services;
 use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class ImageService //bytescale.com
+class ImageService
 {
-    public function saveImage(UploadedFile $image): string
+        private const string BYTE_SCALE_API = 'https://api.bytescale.com/v2/accounts/';
+
+    public function saveImage(UploadedFile $image):? string
     {
-        $response = Http::withBody(
-            file_get_contents($image->getPathName()), 'image/jpeg'
-        )
+        $body = file_get_contents($image->getPathName());
+        $apiKey = config('services.bytescale.api_key');
+        $accountId = config('services.bytescale.account_id');
+        $url = self::BYTE_SCALE_API . $accountId . '/uploads/binary';
+
+        $response = Http::withBody($body, 'image/jpeg')
             ->withHeaders([
-            'Authorization' => 'Bearer '.config('services.bytescale.api_key'),
-        ])
-            ->post('https://api.bytescale.com/v2/accounts/'.config('services.bytescale.account_id').'/uploads/binary');
+                'Authorization' => 'Bearer ' . $apiKey,
+            ])
+            ->post($url);
 
         return $response->json('fileUrl');
     }
